7d7fde874ea391f063d272e63766cc15f2d6148c
Jimmy B. Lomocso Jr <jimmy.lomocso@gmail.com>
2018-05-17 10:39:40 +0800

    Online Doctors

--- app/Http/Controllers/support/HomeCtrl.php
+++ app/Http/Controllers/support/HomeCtrl.php
@@ -30,11 +30,15 @@ class HomeCtrl extends Controller
         $facility_id = $user->facility_id;
         $tmp = User::where('facility_id',$facility_id)
                 ->where('level','doctor');
+        $start = date('Y-m-d 00:00:00');
+        $end = date('Y-m-d 23:59:59');
+
         $data['countDoctors'] = $tmp->count();
         $data['countOnline'] = $tmp->where(function($q){
                 $q->where('login_status','login')
                     ->orwhere('login_status','login_off');
             })
+            ->whereBetween('last_login',[$start,$end])
             ->count();
         $data['countReferral'] = Tracking::where('referred_from',$facility_id)->count();
         return array(
