19f114362446dfb1a5aad4cbdf7995f2eebf2672
Rusel T. Tayong <ruseltayong@gmail.com>
2020-04-18 15:27:43 +0800

    PushUPdates

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@ if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }
 
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-    set_magic_quotes_runtime(0);
-}
-else {
-    ini_set('magic_quotes_runtime', 0);
-}
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
