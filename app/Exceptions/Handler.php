4181145899aef069e767ed705e4b36e035d2f1d3
ruseltayongski <ruseltayong@gmail.com>
2020-06-26 19:42:15 +0800

    PushUpdates

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,9 +44,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($exception instanceof Exception){
+        /*if($exception instanceof Exception){
             return redirect('/');
-        }
+        }*/
         return parent::render($request, $exception);
     }
 
