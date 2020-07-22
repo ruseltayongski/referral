b2afd63d927b0266508561ea4d876fbb7ca7677e
Rusel T. Tayong <ruseltayong@gmail.com>
2020-07-22 11:30:34 +0800

    PushUpdates

--- app/Http/Controllers/Eoc/InventoryController.php
+++ app/Http/Controllers/Eoc/InventoryController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Redirect;
 
 class InventoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function insertInventory(){
         $facility = Facility::get();
         foreach($facility as $row){
