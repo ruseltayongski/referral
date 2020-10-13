3f815f7079ca9bdf5d3ade19ded8b77cb329ef0d
ruseltayongski <ruseltayong@gmail.com>
2020-10-13 10:46:13 +0800

    PushUpdates

--- app/Http/Controllers/doctor/HomeCtrl.php
+++ app/Http/Controllers/doctor/HomeCtrl.php
@@ -22,72 +22,43 @@ class HomeCtrl extends Controller
 
     public function index()
     {
-        /*for($i=1; $i<=12; $i++)
-        {
-            $date = date('Y').'/'.$i.'/01';
-            $startdate = Carbon::parse($date)->startOfMonth();
-            $enddate = Carbon::parse($date)->endOfMonth();
-
-            $referred = Tracking::
-            whereBetween('date_referred',[$startdate,$enddate])
-                ->where('referred_from',$user->facility_id)
-                ->groupBy('code')
-                ->get();
-            $data['referred'][] = count($referred);
-
-            $accepted = Activity::where(function($q){
-                $q->where('status','accepted')
-                    ->orwhere('status','admitted')
-                    ->orwhere('status','arrived');
-            })
-                ->whereBetween('date_referred',[$startdate,$enddate])
-                ->where('referred_to',$user->facility_id)
-                ->groupBy('code')
-                ->get();
-            $data['accepted'][] = count($accepted);
-
-            $redirected = Activity::where(function($q){
-                $q->where('status','redirected')
-                    ->orwhere('status','rejected');
-            })
-                ->whereBetween('date_referred',[$startdate,$enddate])
-                ->where('referred_to',$user->facility_id)
-                ->groupBy('code')
-                ->get();
-            $data['rejected'][] = count($redirected);
-        }
-
-        $bar_chart = $data;*/
-
-
         ParamCtrl::lastLogin();
 
         $user = Session::get("auth");
-        $group_by_department = User::
-        select(DB::raw("count(users.id) as y"),DB::raw("coalesce(department.description,'NO DEPARTMENT') as label"))
-            ->leftJoin("department","department.id","=","users.department_id")
-            ->where("users.facility_id",$user->facility_id)
-            ->where("users.level","doctor")
-            ->groupBy("users.department_id")
-            ->get();
+        $group_by_department = $user->level == 'admin' ?
+            User::
+            select(DB::raw("count(users.id) as y"),DB::raw("coalesce(department.description,'NO DEPARTMENT') as label"))
+                ->leftJoin("department","department.id","=","users.department_id")
+                ->where("users.level","doctor")
+                ->groupBy("users.department_id")
+                ->get()
+        :
+            User::
+            select(DB::raw("count(users.id) as y"),DB::raw("coalesce(department.description,'NO DEPARTMENT') as label"))
+                ->leftJoin("department","department.id","=","users.department_id")
+                ->where("users.facility_id",$user->facility_id)
+                ->where("users.level","doctor")
+                ->groupBy("users.department_id")
+                ->get()
+        ;
 
         $date_start = Carbon::now()->startOfYear()->format('Y-m-d').' 00:00:00';
         $date_end = Carbon::now()->endOfYear()->format('Y-m-d').' 23:59:59';
 
-        $incoming_statistics = \DB::connection('mysql')->select("call statistics_report_facility('$date_start','$date_end','$user->facility_id')")[0];
+        $incoming_statistics = \DB::connection('mysql')->select("call statistics_report_facility('$date_start','$date_end','$user->facility_id','$user->level')")[0];
         //return json_encode($incoming_statistics);
 
-        $referred_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','referred')");
-        $accepted_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','accepted')");
-        $redirected_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','redirected')");
+        $referred_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','referred','$user->level')");
+        $accepted_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','accepted','$user->level')");
+        $redirected_query = DB::connection('mysql')->select("call doctor_monthly_report('$date_start','$date_end','$user->facility_id','redirected','$user->level')");
 
 
         //for past 10 days
         $date_start_past = date('Y-m-d',strtotime(Carbon::now()->subDays(15))).' 00:00:00';
         $date_end_past = date('Y-m-d',strtotime(Carbon::now()->subDays(1))).' 23:59:59';
-        $referred_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','referred')");
-        $accepted_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','accepted')");
-        $redirected_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','redirected')");
+        $referred_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','referred','$user->level')");
+        $accepted_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','accepted','$user->level')");
+        $redirected_past = DB::connection('mysql')->select("call doctor_past_transaction('$date_start_past','$date_end_past','$user->facility_id','redirected','$user->level')");
 
         $index = 0;
         foreach($referred_query as $row){
