82208f888da6430453cb3c2dc877d83c3dcdce22
ruseltayongski <ruseltayong@gmail.com>
2021-04-27 21:52:28 +0800

    PushUpdates

--- resources/views/vaccine/vaccineview.blade.php
+++ resources/views/vaccine/vaccineview.blade.php
@@ -1464,12 +1464,6 @@
                 @if(!Session::get("ajax"))
                 console.log(token);
                 var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("demo").innerHTML =
-                            this.responseText;
-                    }
-                };
                 xhttp.open("GET", "<?php echo asset('ajax'); ?>"+"/"+token, true);
                 xhttp.send();
                 @endif
