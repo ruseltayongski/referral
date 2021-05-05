3cac099c6a4b5cb212ec6d1da533d3a29c39c920
ruseltayongski <ruseltayong@gmail.com>
2021-05-05 17:07:15 +0800

    PushUpdates

--- resources/views/doctor/patient.blade.php
+++ resources/views/doctor/patient.blade.php
@@ -490,12 +490,12 @@
                     console.log("Status: " + textStatus); console.log("Error: " + errorThrown);
                 }
             });
-            connRef.on('child_added',function(data){
+            /*connRef.on('child_added',function(data){
                 setTimeout(function(){
                     connRef.child(data.key).remove();
                     window.location.reload(false);
                 },500);
-            });
+            });*/
         }else{
             console.log("error else");
             setTimeout(function(){
