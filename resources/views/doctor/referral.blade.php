0816631636138326ff1bcebec5149aad292a3bfc
unknown <Rusel T. Tayong>
2021-07-05 10:07:57 +0800

    asdasd

--- resources/views/doctor/referral.blade.php
+++ resources/views/doctor/referral.blade.php
@@ -17,17 +17,17 @@ $user = Session::get('auth');
     <div class="col-md-9">
         <div class="jim-content">
             @if(count($data) > 0)
-            <div class="alert alert-warning">
-                <div class="text-warning">
-                    <i class="fa fa-warning"></i> Referrals that are not accepted within 72 hours will be <a href="{{ asset('doctor/archived') }}" style="color: #ff405f"> <b><u>archived</u></b></a><br>
-                    <i class="fa fa-warning"></i> Referrals that are not accepted within 30 minutes will get a call from 711 DOH CVCHD HealthLine
+                <div class="alert alert-warning">
+                    <div class="text-warning">
+                        <i class="fa fa-warning"></i> Referrals that are not accepted within 72 hours will be <a href="{{ asset('doctor/archived') }}" style="color: #ff405f"> <b><u>archived</u></b></a><br>
+                        <i class="fa fa-warning"></i> Referrals that are not accepted within 30 minutes will get a call from 711 DOH CVCHD HealthLine
+                    </div>
                 </div>
-            </div>
-            <div class="alert alert-info">
-                <div class="text-info">
-                    <i class="fa fa-info-circle"></i> Incoming patients referred to a particular department can only be accepted by those registered doctors who are assigned in that department.
+                <div class="alert alert-info">
+                    <div class="text-info">
+                        <i class="fa fa-info-circle"></i> Incoming patients referred to a particular department can only be accepted by those registered doctors who are assigned in that department.
+                    </div>
                 </div>
-            </div>
             @endif
             <h3 class="page-header">
                 Incoming Patients
@@ -63,66 +63,66 @@ $user = Session::get('auth');
                                 ?>
                                 <li>
                                     @if($row->status == 'referred' || $row->status == 'seen' || $row->status == 'redirected')
-                                    <i class="fa fa-ambulance bg-blue-active"></i>
-                                    <div class="timeline-item {{ $type }}" id="item-{{ $row->id }}">
-                                        <span class="time"><i class="icon fa {{ $icon }}"></i> <span class="date_activity">{{ $date }}</span></span>
-                                        <h3 class="timeline-header no-border">
-                                            <strong class="text-bold">
-                                                <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
-                                            </strong>
-                                            <small class="status">
-                                                [ {{ $row->sex }}, {{ $row->age }} ]
-                                            </small>
-                                            was <span class="badge bg-blue">referred</span> to
-                                            <span class="text-danger">{{ $department }}</span>
-                                            by <span class="text-warning">{{ $row->referring_md }}</span> of
-                                            <span class="facility">{{ $row->facility_name }}</span>
-                                        </h3> <!-- time line for #referred #seen #redirected -->
-                                        @include('doctor.include.timeline_footer')
-                                    </div>
-                                    @elseif($row->status=='rejected')
-                                    <i class="fa fa-user-times bg-maroon"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
-                                        <h3 class="timeline-header no-border">
-                                            <strong class="text-bold">
-                                                <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
-                                            </strong>
-                                            RECOMMENDED TO REDIRECT to other facility by <span class="text-danger">Dr. {{ $row->action_md }}</span>
-                                        </h3>
-                                        @include('doctor.include.timeline_footer')
-                                    </div>
-                                    @elseif($row->status=='cancelled')
-                                    <i class="fa fa-ban bg-red"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
-                                        <h3 class="timeline-header no-border">
-                                            <strong class="text-bold">
-                                                <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
-                                            </strong>
-                                            was <span class="badge bg-red">{{ $row->status }}</span> by
-                                            {{ $row->referring_md }}
-                                            <br><br>
+                                        <i class="fa fa-ambulance bg-blue-active"></i>
+                                        <div class="timeline-item {{ $type }}" id="item-{{ $row->id }}">
+                                            <span class="time"><i class="icon fa {{ $icon }}"></i> <span class="date_activity">{{ $date }}</span></span>
+                                            <h3 class="timeline-header no-border">
+                                                <strong class="text-bold">
+                                                    <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
+                                                </strong>
+                                                <small class="status">
+                                                    [ {{ $row->sex }}, {{ $row->age }} ]
+                                                </small>
+                                                was <span class="badge bg-blue">referred</span> to
+                                                <span class="text-danger">{{ $department }}</span>
+                                                by <span class="text-warning">{{ $row->referring_md }}</span> of
+                                                <span class="facility">{{ $row->facility_name }}</span>
+                                            </h3> <!-- time line for #referred #seen #redirected -->
                                             @include('doctor.include.timeline_footer')
-                                        </h3>
-                                    </div>
+                                        </div>
+                                    @elseif($row->status=='rejected')
+                                        <i class="fa fa-user-times bg-maroon"></i>
+                                        <div class="timeline-item">
+                                            <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
+                                            <h3 class="timeline-header no-border">
+                                                <strong class="text-bold">
+                                                    <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
+                                                </strong>
+                                                RECOMMENDED TO REDIRECT to other facility by <span class="text-danger">Dr. {{ $row->action_md }}</span>
+                                            </h3>
+                                            @include('doctor.include.timeline_footer')
+                                        </div>
+                                    @elseif($row->status=='cancelled')
+                                        <i class="fa fa-ban bg-red"></i>
+                                        <div class="timeline-item">
+                                            <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
+                                            <h3 class="timeline-header no-border">
+                                                <strong class="text-bold">
+                                                    <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
+                                                </strong>
+                                                was <span class="badge bg-red">{{ $row->status }}</span> by
+                                                {{ $row->referring_md }}
+                                                <br><br>
+                                                @include('doctor.include.timeline_footer')
+                                            </h3>
+                                        </div>
                                     @else
-                                    <i class="fa fa-user-plus bg-olive"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
-                                        <h3 class="timeline-header no-border">
-                                            <strong class="text-bold">
-                                                <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
-                                            </strong>
-                                            was <span class="badge bg-green">{{ $row->status }}</span> by
-                                            <span class="text-success">
+                                        <i class="fa fa-user-plus bg-olive"></i>
+                                        <div class="timeline-item">
+                                            <span class="time"><i class="fa fa-calendar"></i> {{ $date }}</span>
+                                            <h3 class="timeline-header no-border">
+                                                <strong class="text-bold">
+                                                    <a href="{{ asset("doctor/referred")."?referredCode=".$row->code }}" target="_blank">{{ $row->code }}</a>
+                                                </strong>
+                                                was <span class="badge bg-green">{{ $row->status }}</span> by
+                                                <span class="text-success">
                                             Dr. {{ $row->action_md }}
                                             </span>
-                                            <br><br>
-                                            @include('doctor.include.timeline_footer')
-                                        </h3>
-                                    </div>
-                                @endif
+                                                <br><br>
+                                                @include('doctor.include.timeline_footer')
+                                            </h3>
+                                        </div>
+                                    @endif
                                 </li>
                             @endforeach
                         </ul>
@@ -138,11 +138,11 @@ $user = Session::get('auth');
                                         <li>Filer List:</li>
                                         <ul>
                                             @if(isset($search_referral['keyword']))
-                                            <li>Code - {{ $search_referral['keyword'] }}</li>
+                                                <li>Code - {{ $search_referral['keyword'] }}</li>
                                             @endif
                                             <li>Date range - {{ $start.' - '.$end }}</li>
                                             @if(isset($search_referral['department']))
-                                            <li>Department - {{ \App\Department::find($search_referral['department'])->description }}</li>
+                                                <li>Department - {{ \App\Department::find($search_referral['department'])->description }}</li>
                                             @endif
                                         </ul>
                                     </ul>
@@ -178,7 +178,7 @@ $user = Session::get('auth');
 
         function clearFieldsSidebar(){
             <?php
-                Session::put('search_referral',false)
+            Session::put('search_referral',false)
             ?>
             refreshPage();
         }
@@ -192,11 +192,11 @@ $user = Session::get('auth');
         });
 
         @if(Session::get('incoming_denied'))
-            Lobibox.alert("error", //AVAILABLE TYPES: "error", "info", "success", "warning"
+        Lobibox.alert("error", //AVAILABLE TYPES: "error", "info", "success", "warning"
             {
                 msg: "This form was already accepted"
             });
-            <?php Session::put("incoming_denied",false); ?>
+        <?php Session::put("incoming_denied",false); ?>
         @endif
     </script>
 
@@ -249,5 +249,3 @@ $user = Session::get('auth');
     -->
 
 @endsection
-
-
