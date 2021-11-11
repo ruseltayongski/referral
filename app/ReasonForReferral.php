4eee481590ce93c1941cb2a4e53744154a7a5ac7
ChristineAnne <christineannecatubig@gmail.com>
2021-11-11 15:00:34 +0800

    reason for referral crud

---
+++ app/ReasonForReferral.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReasonForReferral extends Model
+{
+    protected $table = 'reason_referral';
+    protected $guarded = array();
+}
