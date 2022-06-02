098df5ac6e9b98b643d6fa308776805ad8e98281
Rusel T. Tayong <ruseltayong@gmail.com>
2022-06-02 16:45:51 +0800

    Fixed Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,8 +2,8 @@
 $dateNow = date('Y-m-d');
 ?>
 <!DOCTYPE html>
-<html>
-  <head lang="en">
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>DOHRO7 Referral System | Log in</title>
