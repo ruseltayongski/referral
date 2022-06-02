76495a55e7bf04c2a9971e30956739dc0668e1be
Rusel T. Tayong <ruseltayong@gmail.com>
2022-06-02 16:56:25 +0800

    Fixed login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,6 @@ $dateNow = date('Y-m-d');
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>DOHRO7 Referral System | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
