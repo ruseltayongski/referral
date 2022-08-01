54e5e1292d8a1e742a1c58b2af381c17f5bb0014
ChristineAnne <christineannecatubig@gmail.com>
2022-08-01 15:41:34 +0800

    changed footer to version  6.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -196,7 +196,7 @@
 <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-arrow-up"></i></button>
 <footer class="footer">
     <div class="container">
-        <p class="text-center">All Rights Reserved {{ date("Y") }} | Version 6.0 </p>
+        <p class="text-center">All Rights Reserved {{ date("Y") }} | Version 6.1 </p>
     </div>
 </footer>
 
