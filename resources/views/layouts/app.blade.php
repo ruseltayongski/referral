cba85b1f1e960ed5ad54be15bf11cef8509e41de
ChristineAnne <christineannecatubig@gmail.com>
2022-08-22 13:14:00 +0800

    edited system version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -198,7 +198,7 @@
 <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-arrow-up"></i></button>
 <footer class="footer">
     <div class="container">
-        <p class="text-center">All Rights Reserved {{ date("Y") }} | Version 6.1 </p>
+        <p class="text-center">All Rights Reserved {{ date("Y") }} | Version 6.2 </p>
     </div>
 </footer>
 
