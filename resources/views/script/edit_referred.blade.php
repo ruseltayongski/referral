88572432169558d8c4bc64115885d6a92c0dfb0d
Rusel T. Tayong <ruseltayong@gmail.com>
2022-11-16 13:24:51 +0800

    PullUpdates

--- resources/views/script/edit_referred.blade.php
+++ resources/views/script/edit_referred.blade.php
@@ -174,10 +174,9 @@
 
     $('.reason_referral').val("<?php echo $reason->id;?>");
     @if(isset($form->other_reason_referral))
-        var other_reason = "{{ $form->other_reason_referral }}";
         $('.reason_referral').val("-1");
         $('#other_reason_referral').html("<textarea class='form-control' id='other_reason' name='other_reason_referral' style='resize: none;width: 100%;' rows='3' required></textarea>");
-        $('#other_reason').val($.parseHTML(other_reason));
+        $('#other_reason').val($.parseHTML("{{ $form->other_reason_referral }}"));
     @else
         $('#other_reason_referral').hide();
         $('#other_reason').val('');
