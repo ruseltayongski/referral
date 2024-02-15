8f2ae1df0a89b9ee7421883dc3999b4f95ff2c7f
Alexis Tacatani <alexisptacatani@gmail>
2024-02-15 17:31:09 +0800

    pushUpdates

---
+++ app/PrescribedPrescription.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DOH-IT Lex
+ * Date: 01/05/2024
+ * Time: 1:31 PM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PrescribedPrescription extends Model
+{
+    protected $table = 'prescribed_prescriptions';
+    //protected $guarded = array();
+
+    protected $fillable = [
+        'prescribed_activity_id',
+        'code',
+        'generic_name',
+        'dosage',
+        'formulation',
+        'brandname',
+        'frequency',
+        'duration',
+        'quantity',
+        'prescribed_status',
+    ];
+
+    public function activity() {
+        return $this -> belongsTo(Activity::class, 'prescribed_activity_id', 'id');
+    }
+    public function activityByCode() {
+        return $this -> belongsTo(Activity::class, 'code', 'code');
+    }
+}
