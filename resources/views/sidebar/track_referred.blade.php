0c6f9e694f53d39ce40d9b343c11b1514ea8a868
gonzalesjhondy <gonzalesjhondy30@gmail.com>
2025-08-29 09:52:28 +0800

    Illegal operator and version to filter redirection page

--- resources/views/sidebar/track_referred.blade.php
+++ resources/views/sidebar/track_referred.blade.php
@@ -5,12 +5,13 @@
     <div class="panel-body">
         <form method="GET" action="{{ url('doctor/referred') }}">
             {{ csrf_field() }}
+            
             <div class="form-group">
                 <input type="text" name="referredCode" value="{{ $referredCode }}" class="form-control" placeholder="Referral Code" />
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-default btn-block">
-                    <i class="fa fa-search"></i> Search
+                    <i class="fa fa-search"></i> Search 
                 </button>
             </div>
         </form>
