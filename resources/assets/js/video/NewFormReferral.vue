c0dcc33ef3eb2be94afc026ccf49d89d17dcfa23
KMLopez2021 <karlmichael.lopez2021@gmail.com>
2025-09-12 13:06:54 +0800

    pushed update

---
+++ resources/assets/js/video/NewFormReferral.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            // Define your component data here
+        };
+    },
+    methods: {
+        // Define your component methods here
+    }
+};
+</script>
+
+<template>
+    <div>
+        <h1>New Form Referral</h1>
+    </div>
+</template>
+
+
+<style scoped>
+    /* Add your component styles here */
+</style>
